dd07e11f22bdfde3a40ff86d380d8821f6773750
Drew <drew@Drews-MacBook.local>
2019-05-30 05:36:40 -0400

    refactored

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,29 +6,52 @@ import SEO from "../components/seo"
 
 const IndexPage = () => (
   <Layout>
-    <SEO title="Home" keywords={[
-      `gatsby`, `application`, `react`, `andy`, `dang`, `trading`, `lifting`, `lifestyle`
-    ]} />
+    <SEO
+      title="Home"
+      keywords={[
+        `gatsby`,
+        `application`,
+        `react`,
+        `andy`,
+        `dang`,
+        `trading`,
+        `lifting`,
+        `lifestyle`,
+      ]}
+    />
 
     <section>
-      <p> Welcome to his personal site. Below are a few projects he's been working on. </p>
+      <p>
+        {" "}
+        Welcome to his personal site. Below are a few projects he's been working
+        on.{" "}
+      </p>
     </section>
 
     <section>
-      <h1><a href="https://andydangtrading.netlify.com/">Trading Blog</a></h1>
-      <p> A blog post leveraging Gatsby and React to showcase his trading journey. </p>
+      <h1>
+        <a href="https://andydangtrading.netlify.com/">Trading Blog</a>
+      </h1>
+      <p>
+        {" "}
+        A blog post leveraging Gatsby and React to showcase his trading journey.{" "}
+      </p>
     </section>
 
     <section>
-      <h1><Link to="/money"> All Money related things</Link></h1>
+      <h1>
+        <Link to="/money"> Tax Calculator</Link>
+      </h1>
       <p> A simple calculator to calculate the amount you'll be taxed </p>
     </section>
 
     <section>
       <h1>Catch of the Day</h1>
-      <p> A app created after following along WesBos's React for Beginners course</p>
+      <p>
+        {" "}
+        A app created after following along WesBos's React for Beginners course
+      </p>
     </section>
-
   </Layout>
 )
 
